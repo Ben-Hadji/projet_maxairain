73883540f5a5bfd1eeedece27aacb78f28872c2c
LamaElkhouk <lamiaelkhoukhi09@gmail.com>
2023-06-26 13:54:43 +0200

    correction conflit de version d'orm

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,12 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use \FaceRecognition\FaceRecognition;
+
+
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'accueil')]
     public function index(): Response
     {
         /**teste */
+
+
         return $this->render('default/index.html.twig');
     }
 }
